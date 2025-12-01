abb50fbad70637f332ae2f8411d851b0ce073cc5
Sainandhan M <sainandhanmucharla@gmail.com>
2025-12-02 01:14:15 +0530

    Movement & Food Update

--- game.js
+++ game.js
@@ -14,7 +14,9 @@ let wallColor = "#342DCA";
 let wallSpaceWidth = oneBlockSize/1.5;
 let wallOffSet = (oneBlockSize - wallSpaceWidth)/2;
 let wallInnerColor = "black";
-let pacman;
+let foodColor = "#FEB897";
+let score =0;
+// let pacman;
 
 const DIRECTION_RIGHT = 4;
 const DIRECTION_UP = 3;
@@ -32,7 +34,7 @@ let map = [
     [1, 1, 1, 1, 1 ,2, 1, 1, 1, 2 ,1, 2, 1, 1, 1 ,2, 1, 1, 1, 1 ,1],
     [0, 0, 0, 0, 1 ,2, 1, 2, 2, 2 ,2, 2, 2, 2, 1 ,2, 1, 0, 0, 0 ,0],
     [1, 1, 1, 1, 1 ,2, 1, 2, 1, 1 ,2, 1, 1, 2, 1 ,2, 1, 1, 1, 1 ,1],
-    [2, 2, 2, 2, 2 ,2, 2, 2, 1, 2 ,2, 2, 1, 2, 2 ,2, 2, 2, 2, 2 ,2],
+    [1, 2, 2, 2, 2 ,2, 2, 2, 1, 2 ,2, 2, 1, 2, 2 ,2, 2, 2, 2, 2 ,1],
     [1, 1, 1, 1, 1 ,2, 1, 2, 1, 2 ,2, 2, 1, 2, 1 ,2, 1, 1, 1, 1 ,1],
     [0, 0, 0, 0, 1 ,2, 1, 2, 1, 1 ,1, 1, 1, 2, 1 ,2, 1, 0, 0, 0 ,0],
     [0, 0, 0, 0, 1 ,2, 1, 2, 2, 2 ,2, 2, 2, 2, 1 ,2, 1, 0, 0, 0 ,0],
@@ -54,12 +56,41 @@ let gameLoop = () =>{
 
 let update = () => {
     pacman.moveProcess();
+    pacman.eat();
 };
+
+let drawFoods = () => {
+    for(let i =0;i<map.length;i++){
+        for(let j=0;j<map[0].length;j++){
+            if(map[i][j] === 2){
+                createRect(
+                    j*oneBlockSize + oneBlockSize/3,
+                    i*oneBlockSize + oneBlockSize/3,
+                    oneBlockSize/3,
+                    oneBlockSize/3,
+                    foodColor
+                );
+            }
+        }
+    }
+}
+
+let drawScore = () => {
+    canvasContext.font = "20px Emulogic";
+    canvasContext.fillStyle = "white";
+    canvasContext.fillText(
+        "Score: " + score,
+        0, 
+        oneBlockSize * (map.length + 1) + 10
+    );
+}
 
 let draw = () =>{
     createRect(0,0,canvas.width,canvas.height,"black");
     drawWalls();
+    drawFoods();
     pacman.draw();
+    drawScore();
 };
 
 let gameInterval = setInterval(gameLoop,1000/fps);
@@ -97,3 +128,19 @@ let createNewPacman = () => {
 
 createNewPacman();
 gameLoop();
+
+window.addEventListener("keydown",(event) => {
+    let k = event.keyCode;
+
+    setTimeout(() => {  
+        if(k === 37 || k === 65){
+            pacman.nextDirection = DIRECTION_LEFT;
+        }else if(k === 38 || k === 87){
+            pacman.nextDirection = DIRECTION_UP;
+        }else if(k === 39 || k === 68){   
+            pacman.nextDirection =  DIRECTION_RIGHT;
+        }else if(k === 40 || k === 83){
+            pacman.nextDirection = DIRECTION_BOTTOM;
+        }
+    }, 100);
+})
--- pacman.js
+++ pacman.js
@@ -6,6 +6,7 @@ class Pacman {
         this.height = height;
         this.speed = speed;
         this.direction = DIRECTION_RIGHT;
+        this.nextDirection =  this.direction;
         this.currentFrame = 1;
         this.frameCount = 7;
 
@@ -23,7 +24,14 @@ class Pacman {
     }
 
     eat(){
-
+        for(let i=0;i<map.length;i++){
+            for(let j=0;j<map[0].length;j++){
+                if(map[i][j] === 2 && this.getMapX() === j && this.getMapY() === i){
+                    map[i][j] = 3;
+                    score++;
+                }
+            }
+        }
     }
 
     moveBackwards(){
@@ -75,6 +83,17 @@ class Pacman {
 
     }
     changeDirectionIfPossible(){
+        if(this.direction === this.nextDirection) return 
+
+        let tempDirection = this.direction;
+        this.direction = this.nextDirection;
+        this.moveForwards();
+        if(this.checkCollision()){
+            this.moveBackwards();
+            this.direction = tempDirection;
+        }else{
+            this.moveBackwards();   
+        }
 
         
     }
@@ -89,7 +108,7 @@ class Pacman {
             this.x + oneBlockSize/2,
             this.y + oneBlockSize/2
         );
-        canvasContext.rotate((this.direction*90*Math.PI)/180);
+        canvasContext.rotate((this.direction * 90 * Math.PI)/180);
         canvasContext.translate(
             -this.x - oneBlockSize/2,
             -this.y - oneBlockSize/2
